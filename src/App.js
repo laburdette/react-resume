d94b915265632e9a06d9c78a1459e4b5414cae0f
laburdette <laburdette@gmail.com>
2020-05-07 19:59:09 -0400

    second try

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Home} from "./home/Home";
 import './App.css';
 
 function App() {
   return (
-    <Home/>
+    <Home />
   );
 }
 
